179130173ad379463e10d0ecd90eb7c83d9eeb09
Alex Kalinins <jmal2424@gmail.com>
2021-11-04 11:22:15 -0400

    Moving Circles Subhero Component

--- components/MovingCircles.jsx
+++ components/MovingCircles.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { motion, useAnimation, useViewportScroll, useTransform } from "framer-motion";
+import { motion, useViewportScroll, useTransform } from "framer-motion";
+import {v4 as uuid} from "uuid";
 
-const colors = [
+
+const COLORS = [
   "#FF0000",
   "#00FF00",
   "#0000FF",
@@ -10,28 +12,33 @@ const colors = [
   "#00FFFF",
 ];
 
+const MIN = 100;
+const MAX = 1000;
+
+const CIRCLE_COUNT = 100;
+
+// array of 10 numbers
+const array = [...Array(CIRCLE_COUNT).keys()];
+
+
 const Circle = () => {
-//   const controls = useAnimation();
-//   React.useEffect(() => {
-//     controls.start({
-//       x: 0,
-//       transition: {
-//         duration: 1,
-//         ease: "easeInOut",
-//       },
-//     });
-//   }, [controls]);
+  let x = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
+  let y = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
+
+  // randomly make x a positive or negative number
+  x *= (Math.random() < 0.5 ? -1 : 1);
+  y *= (Math.random() < 0.5 ? -1 : 1);
 
   const { scrollYProgress } = useViewportScroll();
-  const scale = useTransform(scrollYProgress,[-200,0,10],[0,100,0]);
+  const xTransform = useTransform(scrollYProgress, [0, 1], [0, x]);
+  const yTransform = useTransform(scrollYProgress, [0, 1], [0, y]);
 
   return (
     <motion.div
       className="circle"
-      style={{ backgroundColor: colors[0], translateY: scrollYProgress }}
-    //   initial={{opacity: 0, x: -10}}
-    //     animate={controls}
-    ></motion.div>
+      style={{ borderColor: COLORS[Math.floor(Math.random() * COLORS.length)], translateX: xTransform, translateY: yTransform }}
+
+    />
   );
 };
 
@@ -39,6 +46,11 @@ export default function MovingCircles() {
   return (
     <div className="movingCircles">
       <Circle />
+      {
+        array.map(_=> {
+          return <Circle key={uuid()} />
+        })
+      }
     </div>
   );
 }
