af96a582dd7bf0abc4200d1f8ac5a09a86e0060e
Alex Kalinins <alex.kalinins@mail.utoronto.ca>
2022-04-19 13:08:23 -0400

    Convert thumbnails to tailwind

--- components/ProjectThumbnail.jsx
+++ components/ProjectThumbnail.jsx
@@ -15,20 +15,20 @@ const COLORS = [
 export default function ProjectThumbnail({ data }) {
 
   let MemoImage = memo(() => {
-    return (<img className="max-w-[40vw] h-auto justify-self-start " src={data.image} alt={data.title} />)
+    return (<img className="lg:max-w-[40vw] lg:pt-0 pt-10 h-auto justify-self-start max-w-full" src={data.image} alt={data.title} />)
   }, [data.title, data.image]);
 
 
   return (
-    <div className="projectThumbnail">
-      <div className="subTitleDiv">
-        <h3>{data.title}</h3>
-        {data.timeline && <span>{data.timeline}</span>}
+    <div className="grid lg:grid-cols-2 grid-cols-1 border-2 border-white border-solid rounded-2xl p-12 justify-items-stretch items-center grap-8">
+      <div className="flex flex-col items-start justify-evenly">
+        <h3 className="text-6xl p-0">{data.title}</h3>
+        {data.timeline && <span className="-mt-14 mb-8 text-xl text-left justify-self-start">{data.timeline}</span>}
       </div>
 
-      <div className="tagDiv">
+      <div className="flex flex-row space-x-6">
         {data.tags.map((tag, index) => (
-          <span className="tag" key={uuid()} style={{
+          <span className="tag text-xl" key={uuid()} style={{
             borderColor: COLORS[index % COLORS.length]
           }}>{tag}</span>
         ))}
@@ -38,7 +38,7 @@ export default function ProjectThumbnail({ data }) {
 
       <div className="flex flex-col mx-4">
         <p>{data.description}</p>
-        <div className="thubButtonDiv">
+        <div className="flex flex-row flex-wrap justify-items-start items-center space-x-4">
           {data.github && <SocialButton url={data.github} title="GitHub" />}
           {data.demo && <SocialButton url={data.demo} title="Demo" />}
         </div>
